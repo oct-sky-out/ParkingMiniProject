83a0e88efd613971d78d482c10b9af11ab3df794
oct-sky-out <kms3335k@naver.com>
2022-04-10 18:26:08 +0900

    유저의 정산 테스트

---
+++ src/test/java/com/nhnacademy/user/UserTest.java
@@ -0,0 +1,38 @@
+package com.nhnacademy.user;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import com.nhnacademy.car.Car;
+import com.nhnacademy.car.cartype.CarType;
+import com.nhnacademy.exceptions.LackMoneyException;
+import java.time.LocalDateTime;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class UserTest {
+    User user;
+
+    @Test
+    @DisplayName("유저가 출차 요금을 계산한다.")
+    void user_pay_parking_lot_fee() {
+        Car car = new Car("12A 1111", CarType.NORMAL);
+        long fee = 1000;
+        user = User.normalUser(1000, car, LocalDateTime.now());
+
+        user.payParkingLotFee(fee);
+        assertThat(user.getAmount()).isZero();
+    }
+
+    @Test
+    @DisplayName("유저가 출차 시 요금이 부족하면 예외를 던진다.")
+    void lack_user_money_then_throw_error() {
+        Car car = new Car("12A 1111", CarType.NORMAL);
+        long fee = 1000;
+        user = User.normalUser(560, car, LocalDateTime.now());
+
+        assertThatThrownBy(() -> user.payParkingLotFee(fee))
+            .isInstanceOf(LackMoneyException.class)
+            .hasMessageContaining("잔액", "부족");
+    }
+}
